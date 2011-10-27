9a6c5917e479fd958d14bedf2612c0ee7ade7b76
Paul Holser <pholser@alumni.rice.edu>
2011-10-27 17:52:47 -0500

    more tests to demonstrate matching of theory parms and data points involving generics

--- theories/src/test/java/org/junit/contrib/tests/theories/runner/MatchingGenericParametersTest.java
+++ theories/src/test/java/org/junit/contrib/tests/theories/runner/MatchingGenericParametersTest.java
@@ -2,23 +2,82 @@ package org.junit.contrib.tests.theories.runner;
 
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
+import static org.junit.experimental.results.PrintableResult.testResult;
+import static org.junit.experimental.results.ResultMatchers.isSuccessful;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Test;
 import org.junit.contrib.theories.DataPoint;
 import org.junit.contrib.theories.Theories;
 import org.junit.contrib.theories.Theory;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
-@RunWith(Theories.class)
 public class MatchingGenericParametersTest {
-    @DataPoint public static final List<String> strings = Arrays.asList("what");
-    @DataPoint public static final List<Integer> ints = Arrays.asList(1);
+    @RunWith(Theories.class)
+    public static class CanDecideBetweenDifferentElementTypesOfLists {
+        @DataPoint public static final List<String> strings = Arrays.asList("what");
+        @DataPoint public static final List<Integer> ints = Arrays.asList(1);
 
-    @Theory
-    public void regex(List<String> strings, List<Integer> ints) {
-        assertThat(strings.get(0), is(String.class));
-        assertThat(ints.get(0), is(Integer.class));
+        @Theory
+        public void contentsOfLists(List<String> strings, List<Integer> ints) {
+            assertThat(strings.get(0), is("what"));
+            assertThat(ints.get(0), is(1));
+        }
+    }
+
+    @Test
+    public void differentElementTypes() {
+        assertThat(testResult(CanDecideBetweenDifferentElementTypesOfLists.class), isSuccessful());
+    }
+
+    @RunWith(Theories.class)
+    public static class CanMatchListOfSpecificTypeToListOfUnknownType {
+        @DataPoint public static final List<String> strings = Arrays.asList("what");
+        @DataPoint public static final List<Integer> ints = Arrays.asList(1);
+        public static int count;
+
+        @Theory
+        public void contentsOfLists(List<?> items) {
+            count++;
+        }
+    }
+
+    @Test
+    public void listOfSpecificToListOfUnknown() {
+        assertThat(testResult(CanMatchListOfSpecificTypeToListOfUnknownType.class), isSuccessful());
+        assertEquals(2, CanMatchListOfSpecificTypeToListOfUnknownType.count);
+    }
+
+    @RunWith(Theories.class)
+    public static class CanMatchListOfUpperBoundedTypeToListOfSpecificType {
+        @DataPoint public static final List<Integer> ints = Arrays.asList(1);
+
+        @Theory
+        public void contentsOfLists(List<? extends Number> items) {
+            assertEquals(1, items.get(0));
+        }
+    }
+
+    @Test
+    public void listOfSpecificToListOfUpperBounded() {
+        assertThat(testResult(CanMatchListOfUpperBoundedTypeToListOfSpecificType.class), isSuccessful());
+    }
+
+    @RunWith(Theories.class)
+    public static class CanMatchListOfLowerBoundedTypeToListOfSpecificType {
+        @DataPoint public static final List<Integer> ints = Arrays.asList(1);
+
+        @Theory
+        public void contentsOfLists(List<? super Integer> items) {
+            assertEquals(1, items.get(0));
+        }
+    }
+
+    @Test
+    public void listOfSpecificToListOfLowerBounded() {
+        assertThat(testResult(CanMatchListOfLowerBoundedTypeToListOfSpecificType.class), isSuccessful());
     }
 }
